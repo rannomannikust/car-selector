b868bfa0e08f542fa10a11051d91eeda031405ab
s1blik <ranno.mannikust@gmail.com>
2026-03-18 13:37:15 +0200

    Lisatud PostgreSQL docker-compose ja seadistatud rakenduse ühendus

---
+++ src/main/java/ee/mannikust/carselector/CarSelectorApplication.java
@@ -0,0 +1,13 @@
+package ee.mannikust.carselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSelectorApplication.class, args);
+	}
+
+}
